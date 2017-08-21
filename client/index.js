550eeaa0289e2a687b4abe0c13115b4346ff2882
skimrul <sheikh.imrul@gmail.com>
2017-08-21 11:52:23 +0800

    expose html in port 3000

--- client/index.js
+++ client/index.js
@@ -1,1 +1,2 @@
 
+console.log("Hello in console from client file");
--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
-
 // var express = require('express')
-import express from 'express';
-const app = express()
+import express from "express";
+import open from "open";
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+const app = express();
+const port = 3000;
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
 
+import path from "path";
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../www/index.html"));
+});
+
+
+app.listen(port, function(err) {
+    console.log(`Example app listening on port ${port}`)
+  if (err) {
+    console.log(err);
+  } else {
+    open("http://localhost:" + port);
+  }
+});
