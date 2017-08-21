438b20bcfefa1adbf02b9ef125d069e207fbc2d6
skimrul <sheikh.imrul@gmail.com>
2017-08-21 16:03:38 +0800

    format using prettier

--- server/index.js
+++ server/index.js
@@ -1,18 +1,23 @@
 // var express = require('express')
 import express from "express";
 import open from "open";
-import webpack from 'webpack';
-import config from '../webpack.config.dev';
+import webpack from "webpack";
+import webpackMiddleware from "webpack-dev-middleware";
+import config from "../webpack.config.dev";
 
 const app = express();
 const port = 3000;
 const compiler = webpack(config);
 
-
-app.use(require('webpack-dev-middleware')(compiler, {
+app.use(
+  webpackMiddleware(compiler, {
     noInfo: true,
-    publicPath: config.output.publicPath
-  }));
+    publicPath: config.output.publicPath,
+    stats: {
+		colors: true
+	}
+  })
+);
 
 // app.get('/', function (req, res) {
 //   res.send('Hello World!')
@@ -23,9 +28,8 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../www/index.html"));
 });
 
-
 app.listen(port, function(err) {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
   if (err) {
     console.log(err);
   } else {
